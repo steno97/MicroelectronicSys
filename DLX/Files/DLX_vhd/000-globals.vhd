0a53e8c615c4d740d9018ff1fe85f20eb16c6eec
steno97 <46044807+steno97@users.noreply.github.com>
2021-06-07 15:52:05 +0200

    Update 000-globals.vhd

--- DLX/Files/DLX_vhd/000-globals.vhd
+++ DLX/Files/DLX_vhd/000-globals.vhd
@@ -12,35 +12,35 @@ package myTypes is
     constant FUNC_SIZE    : integer :=  11;                                             -- FUNC field size
 
 -- R-Type instruction -> FUNC field
-	LLS : std_logic_vector(FUNC_SIZE - 1 downto 0) :=  "00000000000";   	-- OR RA,RB,RC func 4
-	LRS : std_logic_vector(FUNC_SIZE - 1 downto 0) :=  "00000000000";   	-- OR RA,RB,RC func 4
+	LLS : std_logic_vector(FUNC_SIZE - 1 downto 0) :=  "00000010000";   	-- OR RA,RB,RC func 4
+	LRS : std_logic_vector(FUNC_SIZE - 1 downto 0) :=  "00000010001";   	-- OR RA,RB,RC func 4
    
-	ADD : std_logic_vector(FUNC_SIZE - 1 downto 0) :=  "00000000000";    -- ADD RS1,RS2,RD func 1
-    SUB : std_logic_vector(FUNC_SIZE - 1 downto 0) :=  "00000000000";    -- SUB RS1,RS2,RD func 2
-	ANDR : std_logic_vector(FUNC_SIZE - 1 downto 0) :=  "00000000000";    -- AND RA,RB,RC  func 3
-	ORR : std_logic_vector(FUNC_SIZE - 1 downto 0) :=  "00000000000";   	-- OR RA,RB,RC func 4
-	XORR : std_logic_vector(FUNC_SIZE - 1 downto 0) :=  "00000000000";   	-- OR RA,RB,RC func 4
-    SNE : std_logic_vector(FUNC_SIZE - 1 downto 0) :=  "00000000000";   	-- OR RA,RB,RC func 4
-    SLE : std_logic_vector(FUNC_SIZE - 1 downto 0) :=  "00000000000";   	-- OR RA,RB,RC func 4
-    SGE : std_logic_vector(FUNC_SIZE - 1 downto 0) :=  "00000000000";   	-- OR RA,RB,RC func 4
+	ADD : std_logic_vector(FUNC_SIZE - 1 downto 0) :=  "00000010010";    -- ADD RS1,RS2,RD func 1
+    SUB : std_logic_vector(FUNC_SIZE - 1 downto 0) :=  "00000010011";    -- SUB RS1,RS2,RD func 2
+	ANDR : std_logic_vector(FUNC_SIZE - 1 downto 0) := "00000010100";    -- AND RA,RB,RC  func 3
+	ORR : std_logic_vector(FUNC_SIZE - 1 downto 0) :=  "00000010101";   	-- OR RA,RB,RC func 4
+	XORR : std_logic_vector(FUNC_SIZE - 1 downto 0) := "00000010110";   	-- OR RA,RB,RC func 4
+    SNE : std_logic_vector(FUNC_SIZE - 1 downto 0) :=  "00000010111";   	-- OR RA,RB,RC func 4
+    SLE : std_logic_vector(FUNC_SIZE - 1 downto 0) :=  "00000011000";   	-- OR RA,RB,RC func 4
+    SGE : std_logic_vector(FUNC_SIZE - 1 downto 0) :=  "00000011001";   	-- OR RA,RB,RC func 4
     
 
 -- I-Type instruction -> OPCODE field
-	NOP : std_logic_vector(OP_CODE_SIZE - 1 downto 0) :=  "000000"; 			-- NOP func 0 
-	BEQZ : std_logic_vector(OP_CODE_SIZE - 1 downto 0) :=  "000000";
-	BNEZ : std_logic_vector(OP_CODE_SIZE - 1 downto 0) :=  "000000";
-	ADDS : std_logic_vector(OP_CODE_SIZE - 1 downto 0) :=  "000000";    -- ADDI1 RS1,RD,INP1  	opcode 5
-	SUBI : std_logic_vector(OP_CODE_SIZE - 1 downto 0) :=  "000000"; 	-- SUBI1 RA,RB,INP1		opcode 6
-	ANDI : std_logic_vector(OP_CODE_SIZE - 1 downto 0) :=  "000000";	-- ANDI1 RA,RB,INP1		opcode 7
-	ORI : std_logic_vector(OP_CODE_SIZE - 1 downto 0) :=  "000000";		-- ORI1 RA,RB,INP1		opcode 8
-	XORI : std_logic_vector(OP_CODE_SIZE - 1 downto 0) :=  "000000";		-- ORI1 RA,RB,INP1		opcode 8
-	SSLI : std_logic_vector(OP_CODE_SIZE - 1 downto 0) :=  "000000";		-- ORI1 RA,RB,INP1		opcode 8
-	SRLI : std_logic_vector(OP_CODE_SIZE - 1 downto 0) :=  "000000";		-- ORI1 RA,RB,INP1		opcode 8
-	SNEI : std_logic_vector(OP_CODE_SIZE - 1 downto 0) :=  "000000";		-- ORI1 RA,RB,INP1		opcode 8
-    SLEI : std_logic_vector(OP_CODE_SIZE - 1 downto 0) :=  "000000";		-- ORI1 RA,RB,INP1		opcode 8
-    SGEI : std_logic_vector(OP_CODE_SIZE - 1 downto 0) :=  "000000";		-- ORI1 RA,RB,INP1		opcode 8
-	LW : std_logic_vector(OP_CODE_SIZE - 1 downto 0) :=  "000000";		-- ORI1 RA,RB,INP1		opcode 8
-	SW : std_logic_vector(OP_CODE_SIZE - 1 downto 0) :=  "000000";		-- ORI1 RA,RB,INP1		opcode 8
+	NOP : std_logic_vector(OP_CODE_SIZE - 1 downto 0) :=   "000000"; 			-- NOP func 0 
+	BEQZ : std_logic_vector(OP_CODE_SIZE - 1 downto 0) :=  "000001";
+	BNEZ : std_logic_vector(OP_CODE_SIZE - 1 downto 0) :=  "000010";
+	ADDS : std_logic_vector(OP_CODE_SIZE - 1 downto 0) :=  "000011";    -- ADDI1 RS1,RD,INP1  	opcode 5
+	SUBI : std_logic_vector(OP_CODE_SIZE - 1 downto 0) :=  "000100"; 	-- SUBI1 RA,RB,INP1		opcode 6
+	ANDI : std_logic_vector(OP_CODE_SIZE - 1 downto 0) :=  "000101";	-- ANDI1 RA,RB,INP1		opcode 7
+	ORI : std_logic_vector(OP_CODE_SIZE - 1 downto 0) :=   "000110";		-- ORI1 RA,RB,INP1		opcode 8
+	XORI : std_logic_vector(OP_CODE_SIZE - 1 downto 0) :=  "000111";		-- ORI1 RA,RB,INP1		opcode 8
+	SSLI : std_logic_vector(OP_CODE_SIZE - 1 downto 0) :=  "001000";		-- ORI1 RA,RB,INP1		opcode 8
+	SRLI : std_logic_vector(OP_CODE_SIZE - 1 downto 0) :=  "001001";		-- ORI1 RA,RB,INP1		opcode 8
+	SNEI : std_logic_vector(OP_CODE_SIZE - 1 downto 0) :=  "001010";		-- ORI1 RA,RB,INP1		opcode 8
+    SLEI : std_logic_vector(OP_CODE_SIZE - 1 downto 0) :=  "001011";		-- ORI1 RA,RB,INP1		opcode 8
+    SGEI : std_logic_vector(OP_CODE_SIZE - 1 downto 0) :=  "001100";		-- ORI1 RA,RB,INP1		opcode 8
+	LW : std_logic_vector(OP_CODE_SIZE - 1 downto 0) :=    "001101";		-- ORI1 RA,RB,INP1		opcode 8
+	SW : std_logic_vector(OP_CODE_SIZE - 1 downto 0) :=    "001110";		-- ORI1 RA,RB,INP1		opcode 8
  
 end myTypes;
 
